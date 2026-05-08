585977763219643d3de1285416e4270d30782184
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-05-08 15:45:58 -0300

    feat: add environments variables

--- recognize-plate-service/main.py
+++ recognize-plate-service/main.py
@@ -1,13 +1,22 @@
+import os
+
+from pathlib import Path
+from dotenv import load_dotenv
+
 from consumer import OCRConsumer
+
+
+ROOT = Path(__file__).resolve().parents[1]
+load_dotenv(ROOT / ".env.idea" if (ROOT / ".env.idea").exists() else ROOT / ".env")
 
 
 def main() -> None:
     consumer = OCRConsumer(
-        host="localhost",
-        port=5672,
-        user="rabbitmq",
-        password="rabbitmq",
-        queue="ocr-processing-queue",
+        host=os.environ["RABBITMQ_HOST"],
+        port=int(os.environ["RABBITMQ_PORT"]),
+        user=os.environ["RABBITMQ_USERNAME"],
+        password=os.environ["RABBITMQ_PASSWORD"],
+        queue=os.environ["RABBITMQ_QUEUE"],
     )
     consumer.start()
 
