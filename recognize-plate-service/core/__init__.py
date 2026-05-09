b4f8fa348455cc3e366e64ee2be2309e1ad1baf2
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-05-09 03:19:11 -0300

    refactor: alter folder use case layer

--- recognize-plate-service/core/__init__.py
+++ recognize-plate-service/core/__init__.py
@@ -4,7 +4,7 @@ from .config.storage import Storage
 from .service.ocr_service import OCRService
 from .service.plate_service import PlateService
 from .service.pre_processor_image_service import PreProcessorImageService
-from .recognize_use_case import RecognizeUseCase
+from .usecase.recognize_use_case import RecognizeUseCase
 
 __all__ = [
     "logger", 
