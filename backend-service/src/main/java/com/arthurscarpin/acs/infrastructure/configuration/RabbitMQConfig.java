2009bf74df3240ba03fdc8abff211089261c6055
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-05-10 23:28:59 -0300

    chore(rabbitmq): configure beans to declare queues on application startup

--- backend-service/src/main/java/com/arthurscarpin/acs/infrastructure/configuration/RabbitMQConfig.java
+++ backend-service/src/main/java/com/arthurscarpin/acs/infrastructure/configuration/RabbitMQConfig.java
@@ -7,10 +7,12 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -36,8 +38,20 @@ public class RabbitMQConfig {
     private String deadRoutingKey;
 
     @Bean
+    public ApplicationRunner rabbitInitializer(RabbitAdmin rabbitAdmin) {
+        return args -> rabbitAdmin.initialize();
+    }
+
+    @Bean
     public JacksonJsonMessageConverter messageConverter() {
         return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        return rabbitAdmin;
     }
 
     @Bean
