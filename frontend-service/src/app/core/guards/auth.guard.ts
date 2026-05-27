f7d1697c347f1ebe196f3f7d8a38cbd13b7fa97e
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-05-27 01:34:09 -0300

    style: format auth guard code for consistency

--- frontend-service/src/app/core/guards/auth.guard.ts
+++ frontend-service/src/app/core/guards/auth.guard.ts
@@ -1,12 +1,12 @@
-import { inject } from "@angular/core";
-import { CanActivateFn, Router } from "@angular/router";
-import { AuthService } from "../services/auth.service";
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = () => {
-    const service = inject(AuthService);
-    const router = inject(Router);
-    if (service.checkLoginStage()) {
-        return true;
-    }
-    return router.createUrlTree(['/login']);
-}
+  const service = inject(AuthService);
+  const router = inject(Router);
+  if (service.checkLoginStage()) {
+    return true;
+  }
+  return router.createUrlTree(['/login']);
+};
